3eb3ca1c5b60177f4cf4d1a8da59325001d21cb0
Angel <angelolguin94@hotmail.com>
2023-05-25 15:31:38 -0700

    cambio

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-    var cnv = createCanvas(1100, 600);
-    var x = (windowWidth - width) / 1.4;
-    var y = (windowHeight - height) / .9;
-    cnv.position(x, y);
-    background(255, 0, 200);
-  }
+  var cnv = createCanvas(1100, 600);
+  var x = (windowWidth - width) / 1.4;
+  var y = (windowHeight - height) / 0.9;
+  cnv.position(x, y);
+  background(255);
+}
 
 function draw() {
   if (mouseIsPressed) {
